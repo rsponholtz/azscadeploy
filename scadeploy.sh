8c6f81e8e10a6924732c4019d3241df37413460f
Ross Sponholtz <rsponholtz@hotmail.com>
2021-11-10 07:42:31 -0800

    created deployment shell script

---
+++ scadeploy.sh
@@ -0,0 +1,10 @@
+terraform init
+terraform plan -out main.tfplan
+terraform apply "main.tfplan"
+terraform output -raw tls_private_key >pkey.out
+chmod 600 pkey.out
+az vm show -d -g scaResourceGroup -n scaVM --query publicIps -o tsv > scaIP.txt
+SCAIP=`cat scaIP.txt`
+SEDCMD="s/20.83.123.73/$SCAIP/g"
+cat inventory.ini.sample | sed $SEDCMD > inventory.ini
+ansible-playbook -i ./inventory.ini sca.yml
