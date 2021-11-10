068c265c94bbff050436e7dd77312f91c6505156
Ross Sponholtz <rsponholtz@hotmail.com>
2021-11-10 12:31:47 -0800

    added retry for ansible

--- scadeploy.sh
+++ scadeploy.sh
@@ -1,6 +1,31 @@
 #!/bin/bash
-export ANSIBLE_HOST_KEY_CHECKING=False
 
+retry() {
+    local -r -i max_attempts="$1"; shift
+    local -r cmd="$@"
+    local -i attempt_num=0
+    local -i resultcode=0
+
+    while  true;  do
+        $cmd
+        resultcode=$?
+        if [[ $resultcode == 0 ]]
+        then
+          return 0
+        fi
+
+        if (( attempt_num == max_attempts ))
+        then
+            echo "Attempt $attempt_num failed and there are no more attempts left!"
+            return 1
+        else
+            echo "Attempt $attempt_num failed! Trying again in $attempt_num seconds..."
+            sleep $(( attempt_num++ ))
+        fi
+    done
+}
+
+export ANSIBLE_HOST_KEY_CHECKING=False
 terraform init
 terraform plan -out main.tfplan
 terraform apply "main.tfplan"
@@ -11,5 +36,6 @@ SCAIP=`cat scaIP.txt`
 SEDCMD="s/20.83.123.73/$SCAIP/g"
 cat inventory.ini.sample | sed $SEDCMD > inventory.ini
 #before running the ansible playbook, wait just a bit
-ansible-playbook -i ./inventory.ini sca.yml
+retry 5 "ansible-playbook -i ./inventory.ini sca.yml"
+resultcode=$?
 echo $?
